10d540cfdaaf665c76718704e47259b29486bb21
Asadbek Gafurov <asadbekgafurov0821@gmail.com>
2026-08-29 20:55:59 +0500

    add points badges and referrals

--- database/models.py
+++ database/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from sqlalchemy import String, BigInteger, Float, ForeignKey, DateTime, Enum
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, BigInteger, Float, ForeignKey, DateTime, Enum, Boolean
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, foreign
 import enum
 
 
@@ -16,6 +16,9 @@ class BookingStatus(str, enum.Enum):
     no_show = "no_show"
     blocked = "blocked"
 
+class ReferralStatus(str, enum.Enum):
+    pending = "pending"
+    completed = "completed"
 
 class User(Base):
     __tablename__ = "users"
@@ -29,11 +32,39 @@ class User(Base):
     language: Mapped[str] = mapped_column(String(5), default="uz")
     trust_score: Mapped[int] = mapped_column(default=100)
     created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    referrals: Mapped[list["Referral"]] = relationship(
+        foreign_keys="Referral.referrer_id"
+    )
+    referral_code: Mapped["ReferralCode | None"] = relationship(
+        back_populates="user",
+        uselist=False
+    )
 
     bookings: Mapped[list["Booking"]] = relationship(
         back_populates="user"
     )
+    points: Mapped["UserPoints | None"] = relationship(
+        back_populates="user",
+        uselist=False
+    )
 
+    points_logs: Mapped[list["PointsLog"]] = relationship(
+    back_populates="user"
+)
+
+    badges: Mapped[list["UserBadge"]] = relationship(
+    back_populates="user"
+)
+
+class UserPoints(Base):
+    __tablename__ = "user_points"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
+    total_points: Mapped[int] = mapped_column(default=0)
+    current_streak: Mapped[int] = mapped_column(default=0)
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    user: Mapped["User"] = relationship(back_populates="points")
 
 class Salon(Base):
     __tablename__ = "salons"
@@ -143,4 +174,68 @@ class Review(Base):
     )
     created_at: Mapped[datetime] = mapped_column(
         default=datetime.utcnow
+    )
+
+class Referral(Base):
+    __tablename__ = "referrals"
+
+    id: Mapped[int] =mapped_column(primary_key=True)
+    referrer_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    referred_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    referrer: Mapped["User"] = relationship(foreign_keys=[referrer_id])
+    referred: Mapped["User"] = relationship(foreign_keys=[referred_id])
+    status: Mapped[ReferralStatus] = mapped_column(
+        Enum(ReferralStatus), default=ReferralStatus.pending
+    )
+    reward_given: Mapped[bool] = mapped_column(Boolean, default=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+
+class ReferralCode(Base):
+    __tablename__ = "referral_codes"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(
+    ForeignKey("users.id"), unique=True
+)
+    code: Mapped[str] = mapped_column(String(8), unique=True)
+    user: Mapped["User"] = relationship(back_populates="referral_code")
+
+class PointsLog(Base):
+    __tablename__ = "points_logs"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    action_type: Mapped[str] = mapped_column(String(50))
+    points: Mapped[int] = mapped_column()
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    user: Mapped["User"] = relationship(
+    back_populates="points_logs"
+)
+
+class Badge(Base):
+    __tablename__ = "badges"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    code: Mapped[str] = mapped_column(String(50), unique=True)
+    name: Mapped[str] = mapped_column(String(100))
+    description: Mapped[str] = mapped_column(String(255))
+    condition_value: Mapped[int] = mapped_column()
+    user_badges: Mapped[list["UserBadge"]] = relationship(
+    back_populates="badge"
+)
+
+
+class UserBadge(Base):
+    __tablename__ = "user_badges"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    badge_id: Mapped[int] = mapped_column(ForeignKey("badges.id"))
+    earned_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    user: Mapped["User"] = relationship(
+        back_populates="badges"
+    )
+
+    badge: Mapped["Badge"] = relationship(
+        back_populates="user_badges"    
     )
